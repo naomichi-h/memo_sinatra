26cd6af26b3bdc3a42b9c0e592897c02fc7d07d8
naomichi-h <naomichi.programming@gmail.com>
2021-05-28 13:41:55 +0900

    rubocopに通し、修正を行いました。

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
@@ -7,36 +9,53 @@ helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
+
+  # メモのIDを元に、該当するハッシュを取り出す
+  def search_hash_with_id(id)
+    files = Dir.glob('data/*')
+    @memos = files.map { |file| JSON.parse(File.read(file)) }
+    @memo = @memos.find { |x| x['id'].include?(id) }
+  end
+
+  # 新規メモをJSONファイルとして新規保存する
+  def create_memo(title, content, time)
+    memo = { 'id' => SecureRandom.uuid, 'title' => title, 'content' => content, 'time' => time }
+    File.open("data/memos_#{memo['id']}.json", 'w') do |file|
+      JSON.dump(memo, file)
+    end
+  end
+
+  # 編集済メモをJSONファイルに上書き保存する
+  def edit_memo(id, title, content, time)
+    memo = { 'id' => id, 'title' => title, 'content' => content, 'time' => time }
+    File.open("data/memos_#{@id}.json", 'w') do |file|
+      JSON.dump(memo, file)
+    end
+  end
 end
 
 not_found do
   @title = '404エラー | memo sinatra'
-  erb :error_404
+  erb :error404
 end
 
 get '/memos' do
   @title = 'メモ一覧 | memo sinatra'
-  files = Dir.glob("data/*")
-  #JSON->ハッシュ->配列
-  @memos = files.map {|file| JSON.load(File.read(file))}
-  @memos.sort_by! {|h| h["time"]}
+  files = Dir.glob('data/*')
+  # JSONファイルを全て読み込み、ハッシュの配列にする
+  @memos = files.map { |file| JSON.parse(File.read(file)) }
+  @memos.sort_by! { |h| h['time'] }
   erb :memos
 end
 
 get '/memos/:id' do
   @title = 'メモ詳細 | memo sinatra'
-  #メモのID
+  # メモのID
   @id = params[:id]
-  #メモのIDを元に、該当するハッシュを取り出す
-  files = Dir.glob("data/*")
-  @memos = files.map {|file| JSON.load(File.read(file))}
-  @memo = @memos.find {|x| x["id"].include?(@id)}
 
-  if @memo
-    erb :memo_detail
-  else
-    redirect not_found
-  end
+  search_hash_with_id(@id)
+
+  @memo ? (erb :memo_detail) : (redirect not_found)
 end
 
 get '/memo' do
@@ -45,48 +64,36 @@ get '/memo' do
 end
 
 get '/memos/:id/edit' do
-    @title = 'メモ編集 | memo sinatra'
-    #メモのID
-    @id = params[:id]
-    #メモのIDを元に、該当するハッシュを取り出す
-    files = Dir.glob("data/*")
-    @memos = files.map {|file| JSON.load(File.read(file))}
-    @memo = @memos.find {|x| x["id"].include?(@id)}
+  @title = 'メモ編集 | memo sinatra'
+  # メモのID
+  @id = params[:id]
 
-    if @memo
-      erb :memo_edit
-    else
-      redirect not_found
-    end
+  search_hash_with_id(@id)
+
+  @memo ? (erb :memo_edit) : (redirect not_found)
 end
-
 
 post '/memos' do
   @title = params[:title]
   @content = params[:content]
-  @time = Time.now.strftime("%Y年%m月%d日 %a %H:%M")
+  @time = Time.now.strftime('%Y年%m月%d日 %a %H:%M')
 
-  memo = { "id" => SecureRandom.uuid, "title" => @title, "content"=> @content, "time" => @time }
-  File.open("data/memos_#{memo["id"]}.json", 'w') do |file|
-    JSON.dump(memo, file)
-  end
-  redirect to("memos")
+  create_memo(@title, @content, @time)
+
+  redirect to('memos')
 end
 
 delete '/memos/:id' do
   @id = params[:id]
   File.delete("data/memos_#{@id}.json")
-  redirect to ("/memos")
+  redirect to('/memos')
 end
 
 patch '/memos/:id' do
   @id = params[:id]
   @title = params[:title]
   @content = params[:content]
-  @time = Time.now
-  memo = { "id" => @id, "title" => @title, "content"=> @content, "time" => @time }
-  File.open("data/memos_#{@id}.json", 'w') do |file|
-  JSON.dump(memo, file)
-end
-  redirect to ("/memos/#{@id}")
+  @time = Time.now.strftime('%Y年%m月%d日 %a %H:%M')
+  edit_memo(@id, @title, @content, @time)
+  redirect to("/memos/#{@id}")
 end
